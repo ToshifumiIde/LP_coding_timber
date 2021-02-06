60dea1274dad78b65561c292649d9217775e55ce
ToshifumiIde <toshifumiide198765@gmail.com>
2021-02-06 09:57:50 +0900

    create section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-"use strict"
+"use strict";
 
 {
-  const =
+  const pageTop = document.getElementById("js-pagetop");
+  const ham = document.getElementById("js-ham");
+
+  ham.addEventListener("click", () => {
+    console.log("hamがクリックされました");
+  });
+
+  pageTop.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("クリックされました");
+  });
+  console.log();
 }
