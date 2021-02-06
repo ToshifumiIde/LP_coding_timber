d72f7fa65703b128f0c95c11a4877a605a740e61
ToshifumiIde <toshifumiide198765@gmail.com>
2021-02-06 16:42:36 +0900

    create hamburger menu

--- js/main.js
+++ js/main.js
@@ -3,9 +3,13 @@
 {
   const pageTop = document.getElementById("js-pagetop");
   const ham = document.getElementById("js-ham");
+  const hamLines = document.querySelectorAll(".hamLine");
 
   ham.addEventListener("click", () => {
     console.log("hamがクリックされました");
+    hamLines.forEach((hamLine) => {
+      hamLine.classList.toggle("active");
+    });
   });
 
   pageTop.addEventListener("click", (e) => {
