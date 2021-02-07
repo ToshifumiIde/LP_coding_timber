afb81212bbdf9956317dcd08bdfeed9dac960b56
ToshifumiIde <toshifumiide198765@gmail.com>
2021-02-07 18:54:27 +0900

    create totop function

--- js/main.js
+++ js/main.js
@@ -6,25 +6,36 @@
   const pageTop = document.getElementById("js-pagetop");
   const ham = document.getElementById("js-ham");
   const hamLines = document.querySelectorAll(".hamLine");
+  let aTags = document.getElementsByTagName("a");
+
+  console.log(aTags);
+
+  const toPageTop = () => {
+    let speed = 400;
+  };
 
   ham.addEventListener("click", () => {
-    console.log("hamがクリックされました");
     hamLines.forEach((hamLine) => {
       hamLine.classList.toggle("active");
     });
   });
 
   pageTop.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("クリックされました");
+    // if (hamLines.classList.contain("active")) {
+    hamLines.forEach((hamLine) => {
+      hamLine.classList.remove("active");
+    });
   });
-  console.log();
+  //   e.preventDefault();
+  //   console.log("クリックされました");
+  // });
 
+  //矢印の表示非表示切替
   window.addEventListener("scroll", () => {
+    let nowY = window.pageYOffset;
     const scrollTarget = 80;
-    let scrollDistance = window.pageYOffset;
-    console.log(scrollDistance);
-    if (scrollDistance > scrollTarget) {
+    console.log(nowY);
+    if (nowY > scrollTarget) {
       pageTop.classList.add("active");
     } else {
       pageTop.classList.remove("active");
